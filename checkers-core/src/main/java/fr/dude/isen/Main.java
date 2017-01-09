6cd1d3d5cecb374927710e4336b4eae90a7a5bb4
cdupl <c.duployez@gmail.com>
2017-01-09 14:06:53 +0100

    Main / Test

---
+++ checkers-core/src/main/java/fr/dude/isen/Main.java
@@ -0,0 +1,10 @@
+package fr.dude.isen;
+
+/**
+ * Created by Clement on 09/01/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
