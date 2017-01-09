80be8aff9c5711ef82696fdc48704c2538d58cd3
Pierre Dfc <piierre.d@live.fr>
2017-01-09 14:50:59 +0100

    Sync

---
+++ checkers-core/src/main/java/fr/dude/isen/CheckersGame.java
@@ -0,0 +1,8 @@
+package fr.dude.isen;
+
+/**
+ * Created by Pierre Defache on 09/01/2017.
+ */
+public interface CheckersGame {
+
+}
---
+++ checkers-core/src/main/java/fr/dude/isen/CheckersGameImpl.java
@@ -0,0 +1,20 @@
+package fr.dude.isen;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+/**
+ * Created by Pierre Defache on 09/01/2017.
+ */
+public class CheckersGameImpl implements CheckersGame {
+
+    @Inject
+    @Named("checkers.board.row")
+    public Integer NbRows;
+
+
+    @Inject
+    @Named("checkers.board.column")
+    public Integer NbColumns;
+
+}
