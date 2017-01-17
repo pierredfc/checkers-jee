5482b269e34cf9ded54259b5dcc0558cc1e726ab
cdupl <c.duployez@gmail.com>
2017-01-17 15:38:32 +0100

    CORS Filter
    Enable Cross domain requests on JAX-RS web services (for debug)

---
+++ checkers-web/src/main/java/fr/dude/isen/api/config/CORSFilter.java
@@ -0,0 +1,25 @@
+package fr.dude.isen.api.config;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * Created by Clement on 17/01/2017.
+ */
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(final ContainerRequestContext requestContext,
+                       final ContainerResponseContext cres) throws IOException {
+        cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+        cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+
+    }
+}
