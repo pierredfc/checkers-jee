cded96f7eac1c81b343bf5b280441e10be125bc9
cdupl <c.duployez@gmail.com>
2017-01-23 14:00:06 +0100

    useless tests

--- checkers-web/src/main/java/fr/dude/isen/api/Services.java
+++ checkers-web/src/main/java/fr/dude/isen/api/Services.java
@@ -1,11 +1,7 @@
 package fr.dude.isen.api;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
 
 /**
  * Created by pierredfc on 16/01/2017.
