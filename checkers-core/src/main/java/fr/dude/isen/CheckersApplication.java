93e06c42ef886b616c69ae8c4142f1d8c9f521d8
cdupl <c.duployez@gmail.com>
2017-01-23 09:34:25 +0100

    Json serializer dependency (genson)

--- checkers-core/src/main/java/fr/dude/isen/CheckersApplication.java
+++ checkers-core/src/main/java/fr/dude/isen/CheckersApplication.java
@@ -15,9 +15,8 @@ public class CheckersApplication {
     }
 
     public static CheckersGame launch() {
+
         Injector injector = Guice.createInjector(new CheckersModule());
         return injector.getInstance(CheckersGameImpl.class);
-
-
     }
 }
--- checkers-web/src/main/java/fr/dude/isen/api/HelloWorldService.java
+++ checkers-web/src/main/java/fr/dude/isen/api/HelloWorldService.java
@@ -1,9 +1,18 @@
 package fr.dude.isen.api;
 
+import fr.dude.isen.CheckersApplication;
+import fr.dude.isen.CheckersGame;
+import fr.dude.isen.CheckersGameImpl;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * Created by Clement on 16/01/2017.
@@ -11,11 +20,33 @@ import javax.ws.rs.core.MediaType;
 @Path("hello")
 public class HelloWorldService {
 
+    private class Student {
+        public String name = "Hello";
+        public String firstname = "World";
+    }
+
     @GET
     @Path("/test")
     @Produces(MediaType.APPLICATION_JSON)
-    public String hello() {
-        return "Hello world";
+    public Student hello() {
+        return new Student();
+    }
+
+    @GET
+    @Path("/list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> getList2() {
+        List<String> list = new ArrayList<String>() {{
+            add("Hello"); add("World");
+        }};
+        return list;
+    }
+
+    @GET
+    @Path("/game")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CheckersGameImpl createGame() {
+        return (CheckersGameImpl) CheckersApplication.launch();
     }
 
 }
